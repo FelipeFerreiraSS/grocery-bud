a01d44dfc0399d66db4a527d6c42517cba254d47
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-09-23 21:57:46 -0300

    estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setlist] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: ''})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Deu certo')
+  }
+
+  return(
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>Lsita de compras</h3>
+          <div className="form-control">
+            <input 
+              type="text" 
+              className="grocery" 
+              placeholder="Ex: ovos, leite"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? 'editar' : 'adicionar'}
+            </button>
+          </div>
+        </form>
+        <div className="grocery-container">
+          <List />
+          <button className="clear-btn">limpar itens</button>
+        </div>
+      </section>
+  ) 
 }
 
 export default App
